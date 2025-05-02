6cfe37c7cc08a38d107fd8f53d2b5cc8bc081d82
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-05-02 14:17:48 +0200

    fix

--- build_bare.sh
+++ build_bare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GLVD_API_IMAGE_REPOSITORY=ghcr.io/gardenlinux/glvd-api
+GLVD_API_IMAGE_REPOSITORY=ghcr.io/fwilhe/glvd-api
 GLVD_API_IMAGE_TAG=latest
 
 build () {
@@ -9,7 +9,7 @@ build () {
     SHA_GLVD=$(podman pull -q --arch="$ARCH" $GLVD_API_IMAGE_REPOSITORY:$GLVD_API_IMAGE_TAG)
     SHA_GL=$(podman pull -q --arch="$ARCH" ghcr.io/gardenlinux/gardenlinux:1592)
 
-    ./unbase_oci --exclude exclude --include include --ldd-dependencies --print-tree podman:"$SHA_GL" podman:"$SHA_GLVD" podman:ghcr.io/gardenlinux/glvd-api:latest-linux${ARCH}_bare
+    ./unbase_oci --exclude exclude --include include --ldd-dependencies --print-tree podman:"$SHA_GL" podman:"$SHA_GLVD" podman:ghcr.io/fwilhe/glvd-api:latest-linux${ARCH}_bare
 
 }
 
